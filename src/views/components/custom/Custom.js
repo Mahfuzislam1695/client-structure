4d65e73fc1596701ccd105b1e0a39a4dfc729683
MAHFUZ ISLAM <77238922+Mahfuzislam1695@users.noreply.github.com>
2022-12-03 20:54:14 +0630

    file demo

---
+++ src/views/components/custom/Custom.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Custom = () => {
+    return (
+        <div>
+            this is a custom
+        </div>
+    );
+};
+
+export default Custom;
---
+++ src/views/pages/commonPage/commonPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const commonPage = () => {
+    return (
+        <div>
+            this is commonPage.
+        </div>
+    );
+};
+
+export default commonPage;
